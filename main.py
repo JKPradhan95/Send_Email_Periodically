76de6ebbcb1e07eff147330d46b9cfaae90d6609
JKPradhan95 <168675593+JKPradhan95@users.noreply.github.com>
2024-06-08 10:44:07 +0000

    Send Email Every Day
    Sending Email Everyday at a certain Time

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import yagmail
 import os
 import time
+from datetime import datetime as dt
 
 sender = 'senderdemo@gmail.com'
 receiver = 'receiverdemo@gmail.com'
@@ -18,16 +19,19 @@ password = os.getenv('PASSWORD')
 if not password:
     print("Error: PASSWORD environment variable not set.")
 else:
-    print("PSWD environment variable is set.")
+    print("PASSWORD environment variable is set.")
 
 try:
     while True:
 
-        yag = yagmail.SMTP(user=sender, password=password)
-
-        yag.send(to=receiver, subject=subject, contents=contents)
-        print("Email Sent!")
-        time.sleep(60)
+        now = dt.now()
+        if now.hour == 17 and now.minute == 5 :
+            
+            yag = yagmail.SMTP(user=sender, password=password)
+            
+            yag.send(to=receiver, subject=subject, contents=contents)
+            print("Email Sent!")
+            time.sleep(60)
 
 except Exception as e:
     print(f"An error occurred: {e}")
